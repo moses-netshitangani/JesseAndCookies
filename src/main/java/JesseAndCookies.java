e652a33db18cbc067f868ad1e49133f2117f9023
Moses <netshitanganimoses@gmail.com>
2024-04-25 22:12:47 +0200

    initial commit

---
+++ src/main/java/JesseAndCookies.java
@@ -0,0 +1,38 @@
+class Result {
+    public static int cookies(int k, List<Integer> A) {
+        // edge case: all items already >= k
+        // edge case: all items = (0,0)
+
+        // sort list in asc order
+        // check if first item <= k, else return
+        // pop first two items and apply formula
+        // append result, and increment count
+
+    }
+}
+
+public class JesseAndCookies {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> A = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.cookies(k, A);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
