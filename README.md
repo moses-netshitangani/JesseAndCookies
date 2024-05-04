21a63326651c40f691d2c3ce4a6a5bcc31b29d7d
Moses <netshitanganimoses@gmail.com>
2024-05-04 13:20:04 +0200

    fix bug

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # JesseAndCookies
 Java solution to Hackerrank's Jesse and cookies problem
+
+Input to be supplied via console or terminal in the format:
+**n k l**
+where n = list size, k = desired sweetness, and l = list of values
+
+i.e. `3 7 1 1 1` would represent list size of 3 (`{1, 1, 1}`) and desired sweetness level of 7
--- src/main/java/JesseAndCookies.java
+++ src/main/java/JesseAndCookies.java
@@ -2,31 +2,18 @@ package src.main.java;
 
 import java.io.*;
 import java.util.*;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
 
 class Result {
     public static int cookies(int k, List<Integer> A) {
         // edge case: all items already >= k
-        // edge case: sequence of 0s
 
         // sort list in desc order
-        // check if last item <= k, else return
+        // check if last item <= k and return count, else:
         // pop first two items and apply formula
-        // append result, and increment count
-        // Now consider using a DS that auto sorts on insertion and deletion - TreeMap
-
-        // if set is empty while list isn't, add to set.
+        // append result into separate list to avoid resorting
+        // increment count
         // if set only has one item, and the item < k return -1
-        // sort the set, and remove the first two items from both
-        // apply formula, and add the result to both
-        // increment
-
-        // consider storing new values in a new list instead of resorting the old one every time
-        // do a check to see if the minimum exists in that list too
-        // while loop should check that both lists not empty
-        // this new list, is a queue :)
+        // this separate list, is a queue :)
 
         int count = 0;
         Queue<Long> B = new LinkedList<>();
@@ -38,17 +25,11 @@ class Result {
 
             if (leastSweetCookie >= k) break;
 
-            if (A.size() + B.size() == 1 && leastSweetCookie < k) return -1;
+            if (A.size() + B.size() == 0 && leastSweetCookie < k) return -1;
 
             long secondLeastSweetCookie = getMinimum(A, B);
             long newCookie = leastSweetCookie + (2 * secondLeastSweetCookie);
-
             B.add(newCookie);
-            // 1: 4126s
-            // 780: 2184s
-
-            // s + 2 * ss
-
             count++;
         }
 
@@ -68,7 +49,6 @@ class Result {
                 min = A.remove(listASize - 1);
             }
         } else min = B.remove();
-
         return min;
     }
 }
@@ -76,25 +56,15 @@ class Result {
 public class JesseAndCookies {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        Scanner input = new Scanner(System.in);
+        int size = input.nextInt();
+        int k = input.nextInt();
 
-        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+        List<Integer> A = new ArrayList<>(size);
+        while (A.size() < size) {
+            A.add(input.nextInt());
+        }
 
-        int n = Integer.parseInt(firstMultipleInput[0]);
-
-        int k = Integer.parseInt(firstMultipleInput[1]);
-
-        List<Integer> A = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                .map(Integer::parseInt)
-                .collect(toList());
-
-        int result = Result.cookies(k, A);
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedReader.close();
-        bufferedWriter.close();
+        System.out.println(Result.cookies(k, A));
     }
 }
